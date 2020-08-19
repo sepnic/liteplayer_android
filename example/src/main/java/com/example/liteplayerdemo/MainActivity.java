fa1445e7bf0e57c6b672a813f1a603739d7443c7
sepnic <sysu.zqlong@gmail.com>
2020-08-19 16:12:44 +0800

    Add buttons in demo to control player start/pause/seek/stop
    Signed-off-by: sepnic <sysu.zqlong@gmail.com>

--- example/src/main/java/com/example/liteplayerdemo/MainActivity.java
+++ example/src/main/java/com/example/liteplayerdemo/MainActivity.java
@@ -1,26 +1,66 @@
 package com.example.liteplayerdemo;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.res.AssetManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import androidx.core.app.ActivityCompat;
 
 import com.sepnic.liteplayer.Liteplayer;
 
 public class MainActivity extends Activity {
     private final static String TAG = "LitelayerDemo";
     private Liteplayer mLiteplayer;
-    private boolean mStarted;
-    private boolean mPaused;
+    private TextView mStatusView;
+    private int mStatus;
+
+    private static final int LITEPLAYER_IDLE            = 0x00;
+    private static final int LITEPLAYER_INITED          = 0x01;
+    private static final int LITEPLAYER_PREPARED        = 0x02;
+    private static final int LITEPLAYER_STARTED         = 0x03;
+    private static final int LITEPLAYER_PAUSED          = 0x04;
+    private static final int LITEPLAYER_SEEKCOMPLETED   = 0x05;
+    private static final int LITEPLAYER_CACHECOMPLETED  = 0x06;
+    private static final int LITEPLAYER_NEARLYCOMPLETED = 0x07;
+    private static final int LITEPLAYER_COMPLETED       = 0x08;
+    private static final int LITEPLAYER_STOPPED         = 0x09;
+    private static final int LITEPLAYER_ERROR           = 0x0A;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    /**
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permission then the user will be prompted to
+     * grant permissions
+     */
+    private void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText("Liteplayer demo");
+        verifyStoragePermissions(this);
+
+        mStatusView = findViewById(R.id.statusView);
+        mStatusView.setText("Idle");
+        mStatus = LITEPLAYER_IDLE;
 
         mLiteplayer = new Liteplayer();
         mLiteplayer.setOnIdleListener(mIdleListener);
@@ -30,75 +70,99 @@ public class MainActivity extends Activity {
         mLiteplayer.setOnSeekCompletedListener(mSeekCompletedListener);
         mLiteplayer.setOnCompletedListener(mCompletedListener);
         mLiteplayer.setOnErrorListener(mErrorListener);
-
-        // test begin
-        mLiteplayer.setDataSource("http://ailabsaicloudservice.alicdn.com/player/resources/23a2d715f019c0e345235f379fa26a30.mp3");
-        mLiteplayer.prepareAsync();
     }
 
-    /** Called when the activity is about to be destroyed. */
     @Override
     protected void onDestroy() {
         mLiteplayer.release();
         super.onDestroy();
     }
 
-    @Override
-    protected void onPause() {
-        if (mStarted && !mPaused)
-            mLiteplayer.pause();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mStarted && mPaused)
+    public void onStartClick(View view) {
+        if (mStatus == LITEPLAYER_IDLE) {
+            //String music_url = Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.mp3";
+            String music_url = "http://ailabsaicloudservice.alicdn.com/player/resources/23a2d715f019c0e345235f379fa26a30.mp3";
+            mLiteplayer.setDataSource(music_url);
+            mLiteplayer.prepareAsync();
+        } else if (mStatus == LITEPLAYER_PAUSED || mStatus == LITEPLAYER_SEEKCOMPLETED) {
             mLiteplayer.resume();
+        }
     }
 
-    Liteplayer.OnIdleListener mIdleListener = new Liteplayer.OnIdleListener() {
+    public void onPauseClick(View view) {
+        if (mStatus == LITEPLAYER_STARTED) {
+            mLiteplayer.pause();
+        }
+    }
+
+    public void onSeekClick(View view) {
+        int position = mLiteplayer.getCurrentPosition();
+        int duration = mLiteplayer.getDuration();
+        int seekOffset = position + 10*1000;
+        if (seekOffset >= duration) {
+            Log.e(TAG, "Failed to seek, postion: " + position + "ms, duration: " + duration + "ms");
+            return;
+        }
+        mLiteplayer.seekTo(seekOffset);
+    }
+
+    public void onStopClick(View view) {
+        if (mStatus != LITEPLAYER_IDLE) {
+            mLiteplayer.reset();
+        }
+    }
+
+    private Liteplayer.OnIdleListener mIdleListener = new Liteplayer.OnIdleListener() {
         public void onIdle(Liteplayer p) {
-            mStarted = false;
-            mPaused = false;
+            mStatus = LITEPLAYER_IDLE;
+            mStatusView.setText("Idle");
         }
     };
 
-    Liteplayer.OnPreparedListener mPreparedListener = new Liteplayer.OnPreparedListener() {
+    private Liteplayer.OnPreparedListener mPreparedListener = new Liteplayer.OnPreparedListener() {
         public void onPrepared(Liteplayer p) {
+            mStatus = LITEPLAYER_PREPARED;
+            mStatusView.setText("Prepared");
             p.start();
         }
     };
 
-    Liteplayer.OnStartedListener mStartedListener = new Liteplayer.OnStartedListener() {
+    private Liteplayer.OnStartedListener mStartedListener = new Liteplayer.OnStartedListener() {
         public void onStarted(Liteplayer p) {
-            mStarted = true;
-            mPaused = false;
+            mStatus = LITEPLAYER_STARTED;
+            mStatusView.setText("Started");
         }
     };
 
-    Liteplayer.OnPausedListener mPausedListener = new Liteplayer.OnPausedListener() {
+    private Liteplayer.OnPausedListener mPausedListener = new Liteplayer.OnPausedListener() {
         public void onPaused(Liteplayer p) {
-            mPaused = true;
+            mStatus = LITEPLAYER_PAUSED;
+            mStatusView.setText("Paused");
         }
     };
 
-    Liteplayer.OnSeekCompletedListener mSeekCompletedListener = new Liteplayer.OnSeekCompletedListener() {
+    private Liteplayer.OnSeekCompletedListener mSeekCompletedListener = new Liteplayer.OnSeekCompletedListener() {
         public void onSeekCompleted(Liteplayer p) {
-            if (!mPaused)
+            mStatusView.setText("SeekCompleted");
+            if (mStatus != LITEPLAYER_PAUSED) {
                 p.start();
+            }
         }
     };
 
-    Liteplayer.OnCompletedListener mCompletedListener = new Liteplayer.OnCompletedListener() {
+    private Liteplayer.OnCompletedListener mCompletedListener = new Liteplayer.OnCompletedListener() {
         public void onCompleted(Liteplayer p) {
-            p.reset();
+            mStatus = LITEPLAYER_COMPLETED;
+            mStatusView.setText("Completed");
+            mLiteplayer.reset();
         }
     };
 
-    Liteplayer.OnErrorListener mErrorListener = new Liteplayer.OnErrorListener() {
+    private Liteplayer.OnErrorListener mErrorListener = new Liteplayer.OnErrorListener() {
         public void onError(Liteplayer p, int what, int extra) {
-            p.reset();
+            mStatus = LITEPLAYER_ERROR;
+            mStatusView.setText("Error");
+            mLiteplayer.reset();
         }
     };
 }
